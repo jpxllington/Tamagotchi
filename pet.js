9445281b92583f6c1f58c04b0dd5c6e1b2637c26
glnkvn <golnarkavianii@gmail.com>
2021-09-23 17:46:56 +0100

    add pet class

---
+++ pet.js
@@ -0,0 +1,9 @@
+class Pet{
+    constructor(name,happiness_level,hunger_level){
+        this.name=name;
+        this.happiness_level=happiness_level;
+        this.hunger_level=hunger_level;
+    }
+
+    
+}
